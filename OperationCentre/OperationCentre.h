ad5ab433d680df2aa9a4b697344808d47f25da9c
Yang Xiaozhou <yang_xiaozhou@icloud.com>
2015-03-06 20:44:03 +0800

    start again

---
+++ OperationCentre/OperationCentre.h
@@ -0,0 +1,21 @@
+
+#ifndef OPERATIONCENTER_H
+#define OPERATIONCENTER_H
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include "Parser.h"
+#include "DataStorage.h"
+
+using namespace std;
+
+class OperationCenter{
+
+public:
+
+	void executeCommand(string command, string item);
+
+};
+#endif
