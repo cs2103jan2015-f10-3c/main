b3bc17caf33ac38796d76ac93d0ac1eef4146afb
Yang Xiaozhou <yang_xiaozhou@icloud.com>
2015-03-07 23:03:35 +0800

    start of the local branch

---
+++ Parser/Parser.h
@@ -0,0 +1,17 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include <string>
+#include <vector>
+#include <fstream>
+ 
+using namespace std;
+
+class Parser{
+
+public:
+
+	vector<string> parseInput(string userInput);
+
+};
+#endif
