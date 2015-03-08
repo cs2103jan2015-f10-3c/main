732c8f16a8bbc3798edef311b0102d7361dac2ac
Yang Xiaozhou <yang_xiaozhou@icloud.com>
2015-03-08 23:56:24 +0800

    add the first skeleton consisting all components, if you clone the repository, remember to reconfigure the include path.

---
+++ BlinkList/DataProcessor/DataProcessor.h
@@ -0,0 +1,33 @@
+#ifndef DATAPROCESSOR_H
+#define DATAPROCESSOR_H
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include "Commons.h"
+#include "DataStorage.h"
+
+using namespace std;
+class DataProcessor{
+
+
+public:
+
+	DataProcessor();
+	string showWelcomeMessage();
+	string addTask(Data task);
+	string display(TimeMacro startTime, TimeMacro endTime);
+	string deleteTask(Data task);
+	string clearTask(TimeMacro startTime, TimeMacro endTime);
+	string executeUndo();
+	string searchTask(string word);
+	string editTask(vector<string> infoType, Data task);
+
+	string convertTaskListToString(vector<Data> taskList);
+
+};
+#endif
---
+++ BlinkList/Parser/Parser.h
@@ -0,0 +1,63 @@
+#ifndef PARSER_H
+#define PARSER_H
+#include <string>
+#include <time.h>
+#include "Commons.h"
+
+using namespace std;
+
+class Parser {
+private: 
+	string _command;
+	Data _myData;
+
+	Parser () {
+	}
+
+	Parser (string commandWord, TimeMacro timeMacro, TimeMicro timeMicro, string desc) {
+		_command = commandWord;
+		_myData.updateTimeMacro (timeMacro);
+		_myData.updateTimeMicro (timeMicro);
+		_myData.updateDesc (desc);
+	}
+/*
+	Parser (string commandWord) {
+		_command = commandWord;
+	}
+
+	Parser (TimeMacro timeMacro) {
+		_myData.updateTimemacro (timeMacro);
+	}
+	*/
+	static const int LENGTH_OF_DATE;
+    static const string DATE_FIRST_DIGIT;
+    static const string DATE_SECOND_DIGIT;
+    static const string MONTH_FIRST_DIGIT;
+    static const string MONTH_SECOND_DIGIT;
+    static const string YEAR_FIRST_DIGIT;
+    static const string YEAR_SECOND_DIGIT;
+    static const string YEAR_THIRD_DIGIT;
+    static const string YEAR_FOURTH_DIGIT;
+	static const int LENGTH_OF_STARTING_TIME;
+	static const int LENGTH_OF_TIME_PERIOD;
+	static const string HOUR_FIRST_DIGIT;
+	static const string HOUR_SECOND_DIGIT;
+	static const string MINUTE_FIRST_DIGIT;
+	static const string MINUTE_SECOND_DIGIT;
+	static const int LENGTH_OF_ATTRIBUTE;
+
+public: //dont forget getter
+	Parser parseInput (string userInput);
+	string extractCommandWord (string userInput);
+	Parser ParseAdd (string userInput, string commandWord, Parser returnInput);
+	Parser ParseEdit (string userInput, string commandWord, Parser returnInput);
+	TimeMacro parseDate (string inputToBeParsesd);
+	TimeMicro parseTime (string inputToBeParsed);
+	string parseTaskNo (string inputToBeParsed);
+	bool isDate (string inputToBeParsed);
+	string convertDateToDayOfTheWeek (int date, int month, int year);
+	bool isStartingTime (string inputToBeParsed);
+	bool isTimePeriod (string inputToBeParsed);
+	bool searchSubstring (string string, char substring);
+};
+#endif
