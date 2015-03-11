ed2b037a17273c231bb9a50c2bb3fa5cf31b5e80
Yang Xiaozhou <yang_xiaozhou@icloud.com>
2015-03-11 22:18:37 +0800

    update "displayTask" API name add in converDataObjectToString function API declaration

--- DataProcessor/DataProcessor.h
+++ DataProcessor/DataProcessor.h
@@ -20,7 +20,7 @@ public:
 	DataProcessor();
 	string showWelcomeMessage();
 	string addTask(Data task);
-	string display(TimeMacro startTime, TimeMacro endTime);
+	string displayTask(TimeMacro startTime, TimeMacro endTime);
 	string deleteTask(int number);
 	string clearTask(TimeMacro startTime, TimeMacro endTime);
 	string executeUndo();
@@ -28,6 +28,7 @@ public:
 	string editTask(int taskNumber, Data task);
 
 	string convertTaskListToString(vector<Data> taskList);
+	string convertDataObjectToString(Data task);
 	//new function
 	string getClearMessage(TimeMacro startTime, TimeMacro endTime);
 	string getEditMessage(Data uneditedTask);
